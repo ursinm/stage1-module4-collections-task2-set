7d083a08184d353ef5af800644722486c19775ae
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:35:09 +0300

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,19 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
+
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> squaresSet = new TreeSet<>();
+
+        // Добавляем квадраты всех элементов в TreeSet
+        for (Integer number : sourceList) {
+            squaresSet.add(number * number);
+        }
+
+        // Возвращаем подмножество в заданном диапазоне
+        return squaresSet.subSet(lowerBound, true, upperBound, true);
     }
 }
